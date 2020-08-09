847339384de3ee827415cbef6a66b36886ddae80
Youngchae Na <youngchaena@naver.com>
2020-08-09 22:47:04 +0900

    success to upload via Typora

--- uploader.py
+++ uploader.py
@@ -1,15 +1,11 @@
+import sys, os
 import configparser
 import boto3
+import uuid
 
-def create_bucket(client, bucket_name, region):
-    location = {'LocationConstraint': region}
-    client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-    # client.put_bucket_acl(Bucket=bucket_name, ACL="public-read")
-    print(f"Created bucket [{bucket_name}] on {region}")
-    
-
+path = os.getcwd()
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 bucket_name = config['SETTING']['BUCKET_NAME']
 region = config['AWS']['REGION']
 
@@ -18,19 +14,39 @@ s3 = boto3.client(
     aws_access_key_id = config['AWS']['ACCESS_KEY'],
     aws_secret_access_key = config['AWS']['SECRET_KEY']
 )
-print("Connection success to AWS S3")
 
-bucket_list = [bucket['Name'] for bucket in s3.list_buckets()['Buckets']] 
-# print(bucket_list)
+def have_bucket():
+    bucket_list = [bucket['Name'] for bucket in s3.list_buckets()['Buckets']] 
+    if bucket_name not in bucket_list:
+        print(f"Cannot find Bucket: {bucket_name}")
+        return False
+    return True
 
-if config['SETTING']['BUCKET_NAME'] not in bucket_list:
-    print(f"Cannot find Bucket: {bucket_name}")
-    create_bucket(s3, bucket_name, region)
+def create_bucket():
+    location = {'LocationConstraint': region}
+    s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+    print(f"Created bucket [{bucket_name}] on {region}")
 
-# image = open('python_logo.png', 'rb')
-filename = 'python_logo.png'
-s3.upload_file(filename, bucket_name, filename)
-s3.put_object_acl(ACL='public-read', Bucket=bucket_name, Key=filename)
-url = f'https://{bucket_name}.s3.{region}.amazonaws.com/{filename}'
-print(f"Uploaded {filename}")
-print(f"url : {url}")
+def upload(filename):
+    f = filename.split('/')[-1]
+    extension = f.split('.')[-1]
+    f = '.'.join(f.split('.')[:-1])
+    f = f"{f}-{str(uuid.uuid4())[:4]}.{extension}"
+    
+    s3.upload_file(Filename=filename, Bucket=bucket_name, Key=f)
+    s3.put_object_acl(ACL='public-read', Bucket=bucket_name, Key=f)
+    url = f'https://{bucket_name}.s3.{region}.amazonaws.com/{f}'
+    return url
+
+def upload_files(files):
+    files.pop(0)
+    for file in files:
+        if file == None:
+            break
+        print(upload(file))
+
+    
+if __name__ == "__main__":
+    if not have_bucket():
+        create_bucket()
+    upload_files(sys.argv)
